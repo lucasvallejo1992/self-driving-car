e41242bfc0a4f138d54628e4cac0f8f43a7bc242
Lucas Vallejo <97030519+lucasvallejo1992@users.noreply.github.com>
2024-03-13 11:48:59 +0100

    feat: lane movement

--- road.js
+++ road.js
@@ -11,6 +11,30 @@ class Road {
         const infinity = 1000000;
         this.top = -infinity;
         this.bottom = infinity;
+
+        const topLeft = {x: this.left, y: this.top};
+        const topRight = {x: this.right, y: this.top};
+        const bottomLeft = {x: this.left, y: this.bottom};
+        const bottomRight = {x: this.right, y: this.bottom};
+
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ]
+    }
+
+    getLaneCenter(laneIndex) {
+        const laneWidth = this.width / this.laneCount;
+
+        console.table({
+            left: this.left,
+            right: this.right,
+            width: this.width,
+            laneWidth: laneWidth,
+            laneIndex: laneIndex
+        })
+
+        return this.left + laneWidth / 2 + Math.min(laneIndex, this.laneCount - 1) * laneWidth;
     }
 
     draw(ctx) {
@@ -18,13 +42,23 @@ class Road {
         ctx.strokeStyle = 'white';
         ctx.beginPath();
 
-        for (let i = 0; i <= this.laneCount; i++) {
+        for (let i = 1; i <= this.laneCount - 1; i++) {
             const x = linearInterpolation(this.left, this.right, i / this.laneCount);
+
+            ctx.setLineDash([20, 20]);
             ctx.beginPath();
             ctx.moveTo(x, this.top);
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
         }
+
+        this.borders.forEach(([start, end]) => {
+            ctx.setLineDash([]);
+            ctx.beginPath();
+            ctx.moveTo(start.x, start.y);
+            ctx.lineTo(end.x, end.y);
+            ctx.stroke();
+        });
     }
 
 }
